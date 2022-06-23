3c4210a2aa27b4273d9227d98e7456b47473866a
Victor <victorsireis@gmail.com>
2022-06-23 15:23:20 -0300

    https://leetcode.com/problems/running-sum-of-1d-array/

---
+++ RunningSum1dArray.cs
@@ -0,0 +1,14 @@
+﻿namespace AlgorithmsExercises;
+
+public class RunningSum1dArray
+{
+    public int[] RunningSum(int[] nums) {
+        
+        for (var i = 1; i < nums.Length; i++)
+        {
+            nums[i] = nums[i - 1] + nums[i];
+        }
+        
+        return nums;        
+    }
+}
