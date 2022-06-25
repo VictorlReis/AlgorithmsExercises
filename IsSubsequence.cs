75bdd6a0c53975601315708cff723911d75c52ef
Victor <victorsireis@gmail.com>
2022-06-25 19:48:24 -0300

    https://leetcode.com/problems/is-subsequence/

---
+++ IsSubsequence.cs
@@ -0,0 +1,18 @@
+﻿namespace AlgorithmsExercises;
+
+public class IsSubsequence
+{
+    public bool Subsequence(string s, string t)
+    {
+        int i = 0, j = 0;
+
+        while (i < s.Length && j < t.Length)
+        {
+            if(s[i] == t[j]) i++;
+            
+            j++;
+        }
+
+        return i == s.Length;
+    }
+}
