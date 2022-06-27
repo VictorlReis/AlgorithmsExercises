3d1e06fac9477844cab6c502feb7f18c91e1f064
Victor <victorsireis@gmail.com>
2022-06-27 00:51:58 -0300

    https://leetcode.com/problems/valid-parentheses/

---
+++ ValidParentheses.cs
@@ -0,0 +1,39 @@
+﻿namespace AlgorithmsExercises;
+
+public class ValidParentheses
+{
+    public bool IsValid(string s)
+    {
+        var b1 = "{}";
+        var b2 = "[]";
+        var b3 = "()";
+
+        int v1 = 0, v2 = 0, v3 = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == b1[0])
+            {
+                v1 = 1;
+                continue;
+            }
+            if (s[i] == b1[1] && v1 == 0) return false;
+            
+            if (s[i] == b2[0])
+            {
+                v2 = 1;
+                continue;
+            }
+            if (s[i] == b2[1] && v2 == 0) return false;
+            
+            if (s[i] == b3[0])
+            {
+                v3 = 1;
+                continue;
+            }
+            if (s[i] == b3[1] && v3 == 0) return false;
+        }
+
+        return true;
+    }
+}
