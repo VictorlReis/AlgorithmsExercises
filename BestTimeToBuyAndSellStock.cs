644645285f9b1ad893000335570b4728f7c70467
Victor <victorsireis@gmail.com>
2022-06-27 20:34:41 -0300

    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/

---
+++ BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,18 @@
+﻿namespace AlgorithmsExercises;
+
+public class BestTimeToBuyAndSellStock
+{
+    public int MaxProfit(int[] prices)
+    {
+        var ret = 0;
+        var i = 0;
+
+        for (var j = 1; j < prices.Length; j++)
+        {
+            if (prices[i] >= prices[j]) i = j;
+            if (prices[j] - prices[i] > ret) ret = prices[j] - prices[i];
+        }
+
+        return ret;
+    }
+}
