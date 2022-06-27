2266819c80c48bcb462358a8ce83d068d2d9a5cd
Victor <victorsireis@gmail.com>
2022-06-27 18:39:47 -0300

    https://leetcode.com/problems/reverse-linked-list/submissions/

---
+++ ReverseLinkedList.cs
@@ -0,0 +1,20 @@
+﻿namespace AlgorithmsExercises;
+
+public class ReverseLinkedList
+{
+    ListNode ReverseList(ListNode head)
+    {
+        ListNode prev = null;
+        while (head is not null)
+        {
+            var control = head.next;
+            head.next = prev;
+            prev = head;
+            head = control;
+        }
+
+        return prev;
+    }
+
+    //ReverseList(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, null))))));
+}
