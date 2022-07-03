77857637ed95b936d3181a27a6715516f08b76a5
Victor <victorsireis@gmail.com>
2022-07-03 12:58:39 -0300

    https://www.hackerrank.com/challenges/compare-the-triplets/

---
+++ CompareTriplets.cs
@@ -0,0 +1,23 @@
+﻿namespace AlgorithmsExercises;
+
+public class CompareTriplets
+{
+    public static List<int> Compare_Triplets(List<int> a, List<int> b)
+    {
+        var alice = 0;
+        var bob = 0;
+
+        for (var i = 0; i < a.Count; i++)
+        {
+            if(a[i] > b[i])
+            {
+                alice++;
+            } else if(a[i] < b[i])
+            {
+                bob++;
+            }
+        }
+
+        return new List<int> { alice, bob };
+    }
+}
