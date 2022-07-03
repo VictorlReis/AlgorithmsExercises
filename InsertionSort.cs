18ac27b2b9f6f17b0a6fd025898305e0497a8ed0
Victor <victorsireis@gmail.com>
2022-07-03 14:56:23 -0300

    InsertionSort

---
+++ InsertionSort.cs
@@ -0,0 +1,22 @@
+﻿namespace AlgorithmsExercises;
+
+public class InsertionSort
+{
+    static List<int> insertionSort(List<int> arr)
+    {
+        for (var i = 0; i < arr.Count - 1; i++)
+        {
+            for (var j = i + 1; j > 0; j--)
+            {
+                if (arr[j - 1] > arr[j])
+                {
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;     
+    }
+
+}
