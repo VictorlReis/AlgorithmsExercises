6e90e55214cb514c5653a2f7b55f349595a7b1bc
Victor <victorsireis@gmail.com>
2022-07-03 12:56:21 -0300

    https://www.hackerrank.com/challenges/a-very-big-sum/

---
+++ VeryBigSum.cs
@@ -0,0 +1,9 @@
+﻿namespace AlgorithmsExercises;
+
+public class VeryBigSum
+{
+    public static long aVeryBigSum(List<long> ar)
+    {
+        return ar.Aggregate((result, item) => result + item);
+    }
+}
