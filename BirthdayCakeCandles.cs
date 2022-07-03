a6370b932a2b23d5aeb9471418240eb499edf824
Victor <victorsireis@gmail.com>
2022-07-03 12:48:09 -0300

    https://www.hackerrank.com/challenges/birthday-cake-candles/

---
+++ BirthdayCakeCandles.cs
@@ -0,0 +1,23 @@
+﻿namespace AlgorithmsExercises;
+
+public class BirthdayCakeCandles
+{
+    public static int birthdayCakeCandles(List<int> candles)
+    {
+        var higher = 0;
+        var cont = 0;
+
+        candles.ForEach(x =>
+        {
+            if (x < higher) return;
+            higher = x;
+        });
+
+        candles.ForEach(x =>
+        {
+            if (x == higher) cont++;
+        });
+        
+        return cont;
+    }
+}
