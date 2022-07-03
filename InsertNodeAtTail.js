93d5b243d31a33d18dcbec5ffeae3e797271dd2b
Victor <victorsireis@gmail.com>
2022-07-03 15:56:55 -0300

    https://www.hackerrank.com/challenges/insert-a-node-at-the-tail-of-a-linked-list/

---
+++ InsertNodeAtTail.js
@@ -0,0 +1,18 @@
+﻿function insertNodeAtTail(head, data) {
+    let newNode = new SinglyLinkedListNode(data);
+
+    if(!head) {
+        head = newNode;
+        return head;
+    }
+
+    let c = head;
+
+    while(c.next) {
+        c = c.next;
+    }
+
+    c.next = newNode
+
+    return head;
+}
