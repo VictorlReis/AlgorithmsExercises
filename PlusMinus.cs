3843669beddcfcc8ac80febd87dd884dab27eb72
Victor <victorsireis@gmail.com>
2022-07-03 12:53:47 -0300

    https://www.hackerrank.com/challenges/plus-minus/

---
+++ PlusMinus.cs
@@ -0,0 +1,36 @@
+﻿namespace AlgorithmsExercises;
+
+public class PlusMinus
+{
+    public static void plusMinus(List<int> arr)
+    {
+        float positive = 0;
+        float negative = 0;
+        float zero = 0;
+
+        arr.ForEach(i =>
+        {
+            switch (i)
+            {
+                case > 0:
+                    positive++;
+                    break;
+                case < 0:
+                    negative++;
+                    break;
+                case 0:
+                    zero++;
+                    break;
+            }
+        });
+
+        var positiveRatio = positive / arr.Count;
+        var negativeRatio = negative / arr.Count;
+        var zeroRatio = zero / arr.Count;
+
+        Console.WriteLine(positiveRatio);
+        Console.WriteLine(negativeRatio);
+        Console.WriteLine(zeroRatio);
+
+    }
+}
