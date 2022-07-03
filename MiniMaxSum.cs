b0b4524e31d80eced9bc39d67b3d67d0910be2c5
Victor <victorsireis@gmail.com>
2022-07-03 12:37:00 -0300

    https://www.hackerrank.com/challenges/mini-max-sum/problem

---
+++ MiniMaxSum.cs
@@ -0,0 +1,27 @@
+﻿namespace AlgorithmsExercises;
+
+public class MiniMaxSum
+{
+    public static void miniMaxSum(List<int> arr)
+    {
+        long somaMenor = 0;
+        long somaMaior = 0;
+
+        arr.Sort();
+        for (int i = 0; i <= arr.Count - 1; i++)
+        {
+            if(i > 0)
+            {
+                somaMaior += arr[i];
+            }
+
+            if( i < arr.Count -1)
+            {
+                somaMenor += arr[i];
+            }
+
+        }
+
+        Console.WriteLine($"{somaMenor} {somaMaior}");
+    }
+}
