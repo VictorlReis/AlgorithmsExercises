48dabf9e0cad1754ea82ca3dcff23d4b90498182
Victor <victorsireis@gmail.com>
2022-07-05 23:45:38 -0300

    https://leetcode.com/problems/plus-one

--- PlusOne.js
+++ PlusOne.js
@@ -19,10 +19,10 @@ var plusOne = function(digits) {
 };
 
 var plusOne = function(digits) {
-
     for(let i = digits.length - 1; i >= 0; i--) {
         if(digits[i] < 9) {
-            digits[i] += 1;
+            digits[i] += 1
+            return digits;
         } else {
             digits[i] = 0;
         }
